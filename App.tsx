b4271193d5e5f21863a012b4f9e608bb6f545bbf
Rafael Alves <Rafael.Allves91@gmail.com>
2020-06-12 00:57:03 -0300

    atob error fixed

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import AppLoading from 'expo';
 import Routes from './src/routes'
 import { StatusBar } from 'react-native';
+import {decode, encode} from 'base-64'
+
+if (!global.btoa) {  global.btoa = encode }
+
+if (!global.atob) { global.atob = decode }
+
 
 
 export default function App() {
+  console.disableYellowBox = true;
 
   return (
     <>
